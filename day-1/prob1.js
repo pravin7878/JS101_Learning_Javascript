ac72e47a712d1c533e71204a1d9cc00d70086f8b
pravin7878 <143276463+pravin7878@users.noreply.github.com>
2023-10-22 22:10:49 +0530

    day one task completed now

---
+++ day-1/prob1.js
@@ -0,0 +1,5 @@
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+let name = "masai school"
+console.log(name);
+    name = "A Transformation in Education"
+console.log(name);
---
+++ day-1/prob3.js
@@ -0,0 +1,8 @@
+//problem-3
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+let name = "pravin kumar"
+ age = "20"
+console.log("my name",name,"my age",age)
